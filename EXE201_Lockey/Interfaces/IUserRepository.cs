00d8314245c0f922141d23b0b9cba328c5335e5e
Châu Đoàn <doanchaucbt@gmail.com>
2024-09-20 22:37:00 +0700

    Login - Hash Password

--- EXE201_Lockey/Interfaces/IUserRepository.cs
+++ EXE201_Lockey/Interfaces/IUserRepository.cs
@@ -2,20 +2,16 @@
 
 namespace EXE201_Lockey.Interfaces
 {
-	public interface IUserRepository
-	{
-		ICollection<User> GetUsers();
-		User GetUser(int id);
-		User GetUserByEmail(string email);
-		
+    public interface IUserRepository
+    {
+        ICollection<User> GetUsers();
+        User GetUser(int id);
+        User GetUserByEmail(string email);
+
         bool CreateUser(User user);
-		bool UpdateUser(User user);
-
-
-
-
-		bool DeleteUser(User user);
-		bool Save();
-		bool UserExists(int pokeId);
-	}
+        bool UpdateUser(User user);
+        bool DeleteUser(User user);
+        bool Save();
+        bool UserExists(int pokeId);
+    }
 }
